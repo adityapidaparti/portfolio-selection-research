1955bc047b1be396e4be02ef73969e3fcaecc95e
Adi Pidaparti <pidap004@umn.edu>
2018-04-21 16:09:35 -0500

    Adding in low-beta hyperparameters

--- constants.py
+++ constants.py
@@ -16,5 +16,12 @@ sp500_hyperparameters = {
 	'etas' : [x/1. for x in range(8,32)],
 	'max_risk' : [x/20. for x in range(10,30)]}
 
+# Fine hyperparameters for nyse-o dataset, low betas
+nyse_o_low_betas = {
+	'beta_ranges' : [21, 42, 63, 126, 252],
+	'alphas' : [x/20. for x in range(0,9)],
+	'gammas' : [0.0, 0.001, 0.0025, 0.005, 0.01],
+	'etas' : [x/10. for x in range(0,16,)],
+	'max_risk' : [x/20. for x in range(8,20)]}
 if __name__ == '__main__':
 	raise NotImplementedError()
